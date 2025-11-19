09658933e9ebc3b402e456e6e506d8318635e6a8
Arrudadiego <dca2@cesar.school>
2025-11-19 19:33:17 -0300

    implement api key

--- test_main.py
+++ test_main.py
@@ -6,9 +6,10 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"status": "online", "message": "IsCoolGPT Backend is running!"}
+    # A mensagem aqui tem que ser IGUALZINHA a do main.py novo
+    assert response.json() == {"status": "online", "message": "IsCoolGPT com Gemini AI Ativado!"}
 
 def test_health_check():
     response = client.get("/health")
-    assert response.status_code == 200 
+    assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
